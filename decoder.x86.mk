bf0993b0f7b9f8764b8b21f5d2d644c9ed933312
Tianmi Chen <tianmi.chen@intel.com>
2014-10-09 14:40:36 +0800

    libhevc: enable hevc on CTP
    Change-Id: I8dcc5ff03533cc56403d3db4c9743673ac06108d
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-4900
    Category: aosp improvement
    Domain: Video.Media-framework
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Tianmi Chen <tianmi.chen@intel.com>
    Reviewed-on: https://android.intel.com:443/285627

--- decoder.x86.mk
+++ decoder.x86.mk
@@ -1,4 +1,9 @@
+ifeq ($(TARGET_BOARD_PLATFORM), clovertrail)
+LOCAL_CFLAGS        += -DUSE_CLOVERTRAIL
+libhevcd_cflags_x86 += -DX86 -DDISABLE_AVX2 -m32 -mno-avx -DDEFAULT_ARCH=ARCH_X86_SSSE3
+else
 libhevcd_cflags_x86 += -DX86 -DDISABLE_AVX2 -m32 -msse4.2 -mno-avx -DDEFAULT_ARCH=D_ARCH_X86_SSE42
+endif
 
 libhevcd_inc_dir_x86   +=  $(LOCAL_PATH)/decoder/x86
 libhevcd_inc_dir_x86   +=  $(LOCAL_PATH)/common/x86
@@ -6,7 +11,9 @@ libhevcd_inc_dir_x86   +=  $(LOCAL_PATH)/common/x86
 libhevcd_srcs_c_x86    +=  decoder/x86/ihevcd_function_selector.c
 libhevcd_srcs_c_x86    +=  decoder/x86/ihevcd_function_selector_generic.c
 libhevcd_srcs_c_x86    +=  decoder/x86/ihevcd_function_selector_ssse3.c
+ifneq ($(TARGET_BOARD_PLATFORM), clovertrail)
 libhevcd_srcs_c_x86    +=  decoder/x86/ihevcd_function_selector_sse42.c
+endif
 
 
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_inter_pred_filters_ssse3_intr.c
@@ -25,6 +32,7 @@ libhevcd_srcs_c_x86    +=  decoder/x86/ihevcd_it_rec_dc_ssse3_intr.c
 
 
 
+ifneq ($(TARGET_BOARD_PLATFORM), clovertrail)
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_inter_pred_filters_sse42_intr.c
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_weighted_pred_sse42_intr.c
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_intra_pred_filters_sse42_intr.c
@@ -33,6 +41,7 @@ libhevcd_srcs_c_x86    +=  common/x86/ihevc_itrans_recon_sse42_intr.c
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_16x16_itrans_recon_sse42_intr.c
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_32x32_itrans_recon_sse42_intr.c
 libhevcd_srcs_c_x86    +=  decoder/x86/ihevcd_it_rec_dc_sse42_intr.c
+endif
 
 libhevcd_srcs_c_x86    +=  common/x86/ihevc_tables_x86_intr.c
 
